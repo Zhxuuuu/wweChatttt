fab7a88ca4170bc2e3dc5a9cf9246e31a3cc1aab
zhx <xmg@163.com>
2016-03-12 15:55:16 +0800

    添加了狗类

---
+++ WeChat/WeChat/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  WeChat
+//
+//  Created by admin on 16/3/12.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Dog : CPBaseSettingViewControl
+
+@end
---
+++ WeChat/WeChat/Dog.m
@@ -0,0 +1,15 @@
+//
+//  Dog.m
+//  WeChat
+//
+//  Created by admin on 16/3/12.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+
+
+@end
